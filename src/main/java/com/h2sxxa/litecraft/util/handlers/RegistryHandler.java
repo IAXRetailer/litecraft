9a75e2e7971551975354c2a2728b5ac796d2be5e
H2Sxxa <retailer673@163.com>
2022-06-01 16:38:56 +0800

    Eventsub

--- src/main/java/com/h2sxxa/litecraft/util/handlers/RegistryHandler.java
+++ src/main/java/com/h2sxxa/litecraft/util/handlers/RegistryHandler.java
@@ -3,11 +3,13 @@ package com.h2sxxa.litecraft.util.handlers;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import com.h2sxxa.litecraft.init.ModItem;
 import com.h2sxxa.litecraft.util.IHasModel;
 
+@EventBusSubscriber
 public class RegistryHandler {
     @SubscribeEvent
     public static void onItemRegister(RegistryEvent.Register<Item> event) {
